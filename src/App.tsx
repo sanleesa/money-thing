e02c81231bfd1ad03b8d0bacf5a660e248509cec
sanleesa <sanlee.r.sa@gmail.com>
2024-08-30 07:21:53 +0530

    Added router for login and register pager.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-
+import { Route, Routes } from "react-router-dom";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <div className="bg-slate-700 w-100 h-screen">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
